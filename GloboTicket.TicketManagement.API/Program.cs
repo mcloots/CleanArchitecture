d0898a4ff2b539d99d37ca4be9cf0e4504c3b55b
Michaël Cloots <michael.cloots@thomasmore.be>
2024-03-24 13:16:59 +0100

    Add project files.

---
+++ GloboTicket.TicketManagement.API/Program.cs
@@ -0,0 +1,10 @@
+using GloboTicket.TicketManagement.API;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder
+    .ConfigureServices()
+    .ConfigurePipeline();
+
+await app.ResetDatabaseAsync();
+
+app.Run();
